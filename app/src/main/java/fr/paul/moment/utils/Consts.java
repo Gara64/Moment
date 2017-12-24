4e43a73fef7794a33ef940ce5fa753579c4c1f19
Gara64 <paul@cozycloud.cc>
2017-12-24 19:01:19 +0100

    Add const

--- app/src/main/java/fr/paul/moment/utils/Consts.java
+++ app/src/main/java/fr/paul/moment/utils/Consts.java
@@ -7,6 +7,7 @@ package fr.paul.moment.utils;
 public class Consts {
     public static final String BASE_URL = "";
     public static final String SERVER_SCRIPT = "moment.php";
+    public static final String MANUAL_MOMENT = "manual_moment";
     public static final String CONTENT_TYPE_JSON = "json";
     public static final String CONTENT_TYPE_IMAGE = "image";
 
